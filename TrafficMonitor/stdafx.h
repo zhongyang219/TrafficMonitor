1ecaa9cd044389e7c5abc2b1a2977c6a58aa3acb
Zhong Yang <zhongyang219@hotmail.com>
2018-08-11 16:19:00 +0800

    update

--- TrafficMonitor/stdafx.h
+++ TrafficMonitor/stdafx.h
@@ -81,7 +81,7 @@ using std::ofstream;
 #define MAX_INSERT_TO_TASKBAR_CNT 5		//尝试嵌入任务栏的最大次数
 
 #define VERSION L"1.75"
-#define COMPILE_DATE L"2018/08/07"
+#define COMPILE_DATE L"2018/08/11"
 
 #define MAX_NOTIFY_ICON 4		//可选的通知区图标数量
 
