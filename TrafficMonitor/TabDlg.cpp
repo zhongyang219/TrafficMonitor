5a4b10c9d6cc513cffa620f89f589c33597a34a6
Zhong Yang <zhongyang219@hotmail.com>
2021-12-04 23:22:54 +0800

    在Windows11中将标签页的背景颜色改成和Tab控件标签一致的颜色

--- TrafficMonitor/TabDlg.cpp
+++ TrafficMonitor/TabDlg.cpp
@@ -43,7 +43,10 @@ BOOL CTabDlg::OnInitDialog()
 
 	// TODO:  在此添加额外的初始化
 	//将窗口背景设置成白色
-	SetBackgroundColor(RGB(255, 255, 255));
+    if (theApp.m_win_version.IsWindows11OrLater())
+	    SetBackgroundColor(RGB(249, 249, 249));
+    else
+	    SetBackgroundColor(RGB(255, 255, 255));
 
 	return TRUE;  // return TRUE unless you set the focus to a control
 				  // 异常: OCX 属性页应返回 FALSE
