6c53d02f81fef53908e05ce5b8553a03bf4108b2
Zhong Yang <zhongyang219@hotmail.com>
2022-01-16 10:40:48 +0800

    去掉向注册表中写入“应用程序向导生成的本地应用程序”的代码

--- TrafficMonitor/TrafficMonitor.cpp
+++ TrafficMonitor/TrafficMonitor.cpp
@@ -991,7 +991,7 @@ BOOL CTrafficMonitorApp::InitInstance()
     // 更改用于存储设置的注册表项
     // TODO: 应适当修改该字符串，
     // 例如修改为公司或组织名
-    SetRegistryKey(_T("应用程序向导生成的本地应用程序"));
+    //SetRegistryKey(_T("应用程序向导生成的本地应用程序"));        //暂不使用注册表保存数据
 
     //启动时检查更新
 #ifndef _DEBUG      //DEBUG下不在启动时检查更新
