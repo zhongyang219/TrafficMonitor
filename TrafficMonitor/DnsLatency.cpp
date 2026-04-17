1de75c6bbb62048f1d8eed704f7feb4ee522594d
kfn-d0 <caio.fndo@gmail.com>
2026-04-17 11:58:17 -0300

    Fix missing headers and deprecated warnings for DNS Latency

--- TrafficMonitor/DnsLatency.cpp
+++ TrafficMonitor/DnsLatency.cpp
@@ -1,3 +1,4 @@
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
 #include "stdafx.h"
 #include "DnsLatency.h"
 
--- TrafficMonitor/DnsLatency.h
+++ TrafficMonitor/DnsLatency.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <winsock2.h>
 #include <icmpapi.h>
 #include <iphlpapi.h>
 
